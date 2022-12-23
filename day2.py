2345a81a1338f4ce3b27a1cdf21a2c5c7eec869a
gurdipjheeta <gjheeta@gmail.com>
2022-12-23 10:47:45 +0000

    day 2 finalised

--- day2.py
+++ day2.py
@@ -8,6 +8,7 @@ map_inputs = {'A': 'Rock', 'B': 'Paper', 'C': 'Scissors', 'X': 'Rock', 'Y': 'Pap
 points = {'Rock': 1, 'Paper': 2, 'Scissors': 3}
 game_result = {'Win': 6, 'Lose': 0, 'Draw': 3}
 
+
 def rounds(game_input):
     opponent = map_inputs[game_input[0]]
     mine = map_inputs[game_input[2]]
@@ -18,6 +19,7 @@ def rounds(game_input):
     else:
         return game_result['Win'] + points[mine]
 
+
 results = []
 
 for i in data:
@@ -27,9 +29,25 @@ for i in data:
 print(sum(results))
 
 # Part 2
+# X Lose, Y draw, Z Win
 
 new_mapping_inputs = {'A': 'Rock', 'B': 'Paper', 'C': 'Scissors', 'X': 'Lose', 'Y': 'Draw', 'Z': 'Win'}
 
 def round2(game_input):
     opponent = map_inputs[game_input[0]]
-    strategy = map_inputs[game_input[2]]
+    strategy = new_mapping_inputs[game_input[2]]
+    if (opponent, strategy) in [('Rock', 'Draw'), ('Paper', 'Lose'), ('Scissors', 'Win')]:
+        return game_result[strategy] + points['Rock']
+    elif (opponent, strategy) in [('Rock', 'Win'), ('Paper', 'Draw'), ('Scissors', 'Lose')]:
+        return game_result[strategy] + points['Paper']
+    else:
+        return game_result[strategy] + points['Scissors']
+
+results_round2 = []
+
+for i in data:
+    total = round2(i)
+    results_round2.append(total)
+    print(i)
+
+print(sum(results_round2))
