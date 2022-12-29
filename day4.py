6d196267213ab473ad85def7e0ea39d54ab0c4be
gurdipjheeta <gjheeta@gmail.com>
2022-12-29 11:45:12 +0000

    day 4 part 2

--- day4.py
+++ day4.py
@@ -18,3 +18,13 @@ for r in assignments:
 
 print(number_of_contains)
 
+number_of_overlap = 0
+for assignment_pair in assignments:
+    first_range, second_range = assignment_pair.split(',')
+    start_a, end_a = map(int, first_range.split('-'))
+    start_b, end_b = map(int, second_range.split('-'))
+    if start_a in range(start_b, end_b+1) or end_a in range(start_b, end_b+1) or  start_b in range(start_a, end_a+1) or end_b in range(start_a, end_a+1):
+        number_of_overlap += 1
+
+print(number_of_overlap)
+
