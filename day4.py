d2f109b64ddb2c15b398a8c0e0a99629ac396533
gurdipjheeta <gjheeta@gmail.com>
2022-12-29 11:42:48 +0000

    day 4 part 1

--- day4.py
+++ day4.py
@@ -5,5 +5,16 @@ with open("day4_input.txt", 'r') as f:
     lines = f.readlines()
     assignments = [entry.strip() for entry in lines]
 
-print(assignments)
+def is_range_a_in_range_b(range_a, range_b):
+    start_a, end_a = map(int, range_a.split('-'))
+    start_b, end_b = map(int, range_b.split('-'))
+    return start_b <= start_a and end_a <= end_b
+
+number_of_contains = 0
+for r in assignments:
+    first_range, second_range = r.split(',')
+    if is_range_a_in_range_b(first_range, second_range) or is_range_a_in_range_b(second_range, first_range):
+        number_of_contains += 1
+
+print(number_of_contains)
 
